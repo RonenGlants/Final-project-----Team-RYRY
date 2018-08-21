1417c8a3e8c3cffbb1f4b1edc22f22bfacca77e6
RonenGlants <41534740+RonenGlants@users.noreply.github.com>
2018-08-21 19:54:51 +0300

    last part of the day
    insert (V)
    find (VX)

--- src/RYRYServer/Database/DBManager.js
+++ src/RYRYServer/Database/DBManager.js
@@ -2,11 +2,10 @@ const UsersDBManager = require('./UsersDBManager.js');
 const mongo = require('mongodb').MongoClient;
 const assert = require('assert');
 
-
 module.exports = class DBManager {
     constructor() {
         this.config = {
-            autoIndex: false,
+            autoIndex: true,
             useNewUrlParser: true,
         };
         this.url = "mongodb://localhost:27017/ryryDB";
@@ -21,6 +20,7 @@ module.exports = class DBManager {
             }
 
             var dbase = db.db("ryryDB");
+
             dbase.createCollection("user-data", function (err, collection) {
             });
         }.bind(this));
@@ -29,21 +29,29 @@ module.exports = class DBManager {
     getUsers(){
         mongo.connect(this.url, this.config, function (err, db) {
             assert.equal(null, err);
+
             var dbase = db.db("ryryDB");
             var cursor = this.usersManager.getAllUsers(dbase);
             var finalTable = [];
+
             cursor.forEach(function(itr,err){
                 assert.equal(null, err);
                 finalTable.push(itr);
-            });
-            db.close();
-            return finalTable;
+            }).then(function(value) {
+                    db.close();
+
+                    return finalTable;
+                }
+            ) // todo: what is happening here?????
         }.bind(this))
     }
+
     insertUser(user) {
         var insertOperation = mongo.connect(this.url, this.config, function (err, db) {
             assert.equal(null, err);
+
             var dbase = db.db("ryryDB");
+
             this.usersManager.insertUser(dbase, user);
             db.close();
         }.bind(this))
