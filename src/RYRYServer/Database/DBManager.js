86682acf0a1313a7fd77c15e2bd6ea078689fc8a
RonenGlants <41534740+RonenGlants@users.noreply.github.com>
2018-08-21 18:58:15 +0300

    part 4

--- src/RYRYServer/Database/DBManager.js
+++ src/RYRYServer/Database/DBManager.js
@@ -4,27 +4,35 @@ const assert = require('assert');
 
 
 module.exports = class DBManager {
-    constructor(){
+    constructor() {
+        this.config = {
+            autoIndex: false,
+            useNewUrlParser: true,
+        };
         this.url = "mongodb://localhost:27017/ryryDB";
         this.usersManager = new UsersDBManager();
         this.initDB.bind(this)();
     }
 
-    initDB(){
-        mongo.connect(this.url,function (err,db) {
-        assert.equal(null,err);
-        db.createCollection("user-data",{autoIndexId: true});
-        db.close();
-       }.bind(this));
+    initDB() {
+        mongo.connect(this.url, this.config, function (err, db) {
+            if (err) {
+                return console.dir(err);
+            }
+
+            var dbase = db.db("ryryDB");
+            dbase.createCollection("user-data", function (err, collection) {
+            });
+        }.bind(this));
     }
 
-    insertUser(user){
-            mongo.connect(this.url,function (err,db) {
-            assert.equal(null,err);
-            this.usersManager.insertUser(db,user);
+    insertUser(user) {
+        var insertOperation = mongo.connect(this.url, this.config, function (err, db) {
+            assert.equal(null, err);
+            var dbase = db.db("ryryDB");
+            this.usersManager.insertUser(dbase, user);
             db.close();
         }.bind(this))
-
     }
 }
 
