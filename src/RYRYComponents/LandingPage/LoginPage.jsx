66eb2a9fe4892880609448dd6182edd5eee1b58e
RonenGlants <41534740+RonenGlants@users.noreply.github.com>
2018-09-08 15:02:26 +0300

    handle signup string validation on sumbit

--- src/RYRYComponents/LandingPage/LoginPage.jsx
+++ src/RYRYComponents/LandingPage/LoginPage.jsx
@@ -15,47 +15,55 @@ export default class LoginPage extends React.Component {
     render() {
         return (
             <Row className="login-page-wrapper">
-               <Col sm={{ size: 'auto', offset: 1}}>
-                <img className="login-logo" src={loginLogoImage}/>
-                <form onSubmit={this.handleLogin}>
-                   <InputContainer labelClassName="username-label" labelValue="Name" type="text" inputChangeValidation={this.props.nameChangeValidation}/>
-                    <InputContainer labelClassName="password-label" labelValue="Password" type="password" inputChangeValidation={this.props.passwordChangeValidation}/>
-                    <Button color="primary" className="submit-btn btn" type="submit" value="Login">Login</Button>
-                </form>
+                <Col sm={{size: 'auto', offset: 1}}>
+                    <img className="login-logo" src={loginLogoImage}/>
+                    <form onSubmit={this.handleLogin}>
+                        <InputContainer labelClassName="username-label" labelValue="Name" type="text"
+                                        inputChangeValidation={this.props.nameChangeValidation}/>
+                        <InputContainer labelClassName="password-label" labelValue="Password" type="password"
+                                        inputChangeValidation={this.props.passwordChangeValidation}/>
+                        <Button color="primary" className="submit-btn btn" type="submit" value="Login">Login</Button>
+                    </form>
                 </Col>
-                <Col sm={{ size: 'auto', offset: 1}} className="login-page-not-signed-yet">
-                    <Card body outline color="primary" className="notSignedYetCard" style={{width:"80%", height:"60%"}}>
+                <Col sm={{size: 'auto', offset: 1}} className="login-page-not-signed-yet">
+                    <Card body outline color="primary" className="notSignedYetCard"
+                          style={{width: "80%", height: "60%"}}>
                         <CardHeader tag="h3">Not signed yet?</CardHeader>
                         <CardBody>
                             <CardText>Join us to earn new skills and enrich others</CardText>
                             <CardText>
                                 <small className="text-muted">It only takes a minute...</small>
                             </CardText>
-                            <Button color="danger" onClick={this.signUpButtonClick.bind(this)} className="submit-btn btn" type="submit" value="SignUp">Sign Up</Button>
+                            <Button color="danger" onClick={this.signUpButtonClick.bind(this)}
+                                    className="submit-btn btn" type="submit" value="SignUp">Sign Up</Button>
                         </CardBody>
-                      </Card>
+                    </Card>
                 </Col>
             </Row>
         );
     }
 
-    signUpButtonClick(){
+    signUpButtonClick() {
         this.props.showSignUp();
     }
 
-    handleLogin(eventLoginClick) {
-        eventLoginClick.preventDefault(); // calls console.warn
-       const userName = eventLoginClick.target.elements[0].value; // target is the form elements are the labels
-       const userPassword = eventLoginClick.target.elements[1].value;
-        return fetch('/users/loginUser', {method:'POST', body: JSON.stringify({userName: userName, userPassword: userPassword}), credentials: 'include'})
-            .then(response=> {        // response is the result
-                if (response.ok){      // ok == 200
-                    this.setState(()=> ({errMessage: ""}));
+    handleLogin(loginClickEvent) {
+        loginClickEvent.preventDefault(); // calls console.warn
+        const userName = loginClickEvent.target.elements[0].value; // target is the form elements are the labels
+        const userPassword = loginClickEvent.target.elements[1].value;
+        return fetch('/users/loginUser', {
+            method: 'POST',
+            body: JSON.stringify({userName: userName, userPassword: userPassword}),
+            credentials: 'include'
+        })
+            .then(response => {        // response is the result
+                if (response.ok) {      // ok == 200
+                    this.setState(() => ({errMessage: ""}));
                     this.props.loginSuccessHandler();
                 } else {
                     if (response.status === 403) {
                         //todo: handle error
-                        this.setState(()=> ({errMessage: "User name already exist, please try another one"}));
+                        this.setState(() => ({errMessage: "User name already exist, please try another one"}));
                     }
                     this.props.loginErrorHandler();
                 }
