35202c3e3002c126794645b0f2b3bed1687cb76d
yardenron07 <yarden.ron07@gmail.com>
2018-09-15 11:48:21 +0300

    Merge branch 'Yaron' of https://github.com/RonenGlants/Final-project-----Team-RYRY into Yaron
    add switch button from sign up to sign in

--- src/RYRYComponents/LandingPage/SignUpPage.jsx
+++ src/RYRYComponents/LandingPage/SignUpPage.jsx
@@ -1,39 +1,59 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import SignUpLogoImage from '../Resources/SignUpLogo.jpg';
 import InputContainer from "./Containers/InputContainer.jsx";
 import RadioContainer from "./Containers/RadioContainer.jsx";
 import './Style/LandingPage.css';
 import SkillsInputContainer from './Containers/SkillsInputContainer.jsx';
+import {Button, Card, CardBody, CardHeader,Row, Col} from 'reactstrap';
 
 export default class SignUpPage extends React.Component {
     constructor(args) {
         super(...args);
         this.signUpButtonClick = this.signUpButtonClick.bind(this);
+        this.signInButtonClick = this.signInButtonClick.bind(this);
         this.state = {
             errMessage: '',
         }
     }
 
     render() {
-    var genderOptions = ["Male","Female"];
+        var genderOptions = ["Male", "Female"];
         return (
             <div className="login-page-wrapper">
+                <Row >
+                    <Col sm={{size: 'auto', offset: 1}}>
                 <form className="signup-form" onSubmit={this.signUpButtonClick}>
                     <img className="signup-logo" src={SignUpLogoImage}/>
-                    <InputContainer labelClassName="username-label" labelValue="First Name" type="text" inputChangeValidation={this.props.nameChangeValidation}/>
-                    <InputContainer labelClassName="username-label" labelValue="Last Name" type="text" inputChangeValidation={this.props.nameChangeValidation}/>
-                    <InputContainer labelClassName="password-label" labelValue="Password" type="password" inputChangeValidation={this.props.passwordChangeValidation}/>
-                    <InputContainer labelClassName="mail-label" labelValue="Email" type="email" placeholder="ronen@RYRY.com"/>
+                    <InputContainer labelClassName="username-label" labelValue="First Name" type="text"
+                                    inputChangeValidation={this.props.nameChangeValidation}/>
+                    <InputContainer labelClassName="username-label" labelValue="Last Name" type="text"
+                                    inputChangeValidation={this.props.nameChangeValidation}/>
+                    <InputContainer labelClassName="password-label" labelValue="Password" type="password"
+                                    inputChangeValidation={this.props.passwordChangeValidation}/>
+                    <InputContainer labelClassName="mail-label" labelValue="Email" type="email"
+                                    placeholder="ronen@RYRY.com"/>
                     <RadioContainer choiceTitle="Gender" choiceOptions={genderOptions}/>
                     <SkillsInputContainer skillsTitle="My Skills"/>
                     <SkillsInputContainer skillsTitle="My Desired Skills"/>
                     <input className="submit-btn btn" type="submit" value="Sign Up"/>
                     <label className="errMessage">{this.state.errMessage}</label>
                 </form>
+                    </Col>
+                    <Col>
+                <Card body outline color="primary" className="alredySignedUpCard"
+                      style={{width: "80%", height: "60%"}}>
+                    <CardHeader tag="h3">Already signed up</CardHeader>
+                    <CardBody>
+                        <Button color="danger" onClick={this.signInButtonClick}
+                                className="submit-btn btn" type="submit" value="SignIn">Sign In</Button>
+                    </CardBody>
+                </Card>
+                    </Col>
+                </Row>
             </div>
         );
     }
+
 
     signUpButtonClick(signUpClickEvent) {
         signUpClickEvent.preventDefault(); // calls console.warn
@@ -59,6 +79,10 @@ export default class SignUpPage extends React.Component {
         else {
             this.showSignUpErrorMessage("Please fill all the information as required.");
         }
+    }
+
+    signInButtonClick() {
+        this.props.showLogin();
     }
 
     showSignUpErrorMessage(error) {
