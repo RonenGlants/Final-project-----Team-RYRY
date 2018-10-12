ab543e5f573723b34c40c534916452fd110620ec
roylimor1 <roylimor1@gmail.com>
2018-10-12 14:26:30 +0300

    update feed UI

--- src/RYRYComponents/Containers/NewsfeedContainer.jsx
+++ src/RYRYComponents/Containers/NewsfeedContainer.jsx
@@ -21,8 +21,8 @@ export default class NewsfeedContainer extends React.Component {
                         <div className="feed-wrapper">
                             <Card width="100px">
                                 <CardBody>
-                                    <CardText className="feed-user-name">Name: {feed.userId}</CardText>
                                     {groupName}
+                                    <CardText className="feed-user-name">Name: {feed.userId}</CardText>
                                     <CardText className="feed-text">Message: {feed.feed}</CardText>
                                     <small className="text-muted">Post time {feed.postTime}</small>
                                 </CardBody>
